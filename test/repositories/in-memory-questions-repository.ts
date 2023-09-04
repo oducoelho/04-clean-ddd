9c293f2b8dfbec35c540b23073191d4a4b3ba70c
Eduardo Coelho <eduardocoelho619@gmail.com>
2023-09-04 19:46:11 -0300

    Subdomains & Domain Events - Best Response Stream

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachment-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
@@ -12,6 +13,16 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
   async create(question: Question) {
     this.items.push(question)
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
+  }
+
+  async save(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items[itemIndex] = question
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
   }
 
   async findBySlug(slug: string) {
@@ -22,12 +33,6 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     }
 
     return question
-  }
-
-  async save(question: Question) {
-    const itemIndex = this.items.findIndex((item) => item.id === question.id)
-
-    this.items[itemIndex] = question
   }
 
   async findById(id: string) {
